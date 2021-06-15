fa2b7e88d2d9d86d3400c8497afabd5491d2b998
Igna Oromendia <ignaciooromendia@gmail.com>
2021-06-15 09:16:38 -0300

    Initial Commit

---
+++ Nutrition TableTests/Nutrition_TableTests.swift
@@ -0,0 +1,33 @@
+//
+//  Nutrition_TableTests.swift
+//  Nutrition TableTests
+//
+//  Created by Igna on 15/06/2021.
+//
+
+import XCTest
+@testable import Nutrition_Table
+
+class Nutrition_TableTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
