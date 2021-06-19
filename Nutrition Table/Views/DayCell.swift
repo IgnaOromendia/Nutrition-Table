928ed91101c25ede9f9c7096b98dcbec55120b49
Igna Oromendia <ignaciooromendia@gmail.com>
2021-06-19 19:05:10 -0300

    segundo commi

--- Nutrition Table/Views/DayCell.swift
+++ Nutrition Table/Views/DayCell.swift
@@ -22,58 +22,58 @@ class DayCell: UITableViewCell {
     @IBOutlet weak var im_dinner: UIImageView!
     
     
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//    }
-//
-//    #warning("No implementation of circleView_FoodType")
-//    // Cell genereated in the week view
-//    func setCell(with weekDay:WeekDay, _ color: UIColor?) {
-//        self.selectionStyle = .none
-//        view_day.cornerRadius(de: 25)
-//        view_day.backgroundColor = color
-//        circleView_FoodType.circle = true
-//        setFoodLabels(weekDay)
-//        setImages()
-//        lbl_day.text = weekDay.dateDay + ", " + weekDay.date.dayMonthDate
-//    }
-//
-//    private func setImages() {
-//        im_lunch.image = UIImage(named: "Sun2.png")
-//        im_dinner.image = UIImage(named: "Moon2.png")
-//        im_breakfast.image = UIImage(named: "Sunrise2.png")
-//    }
-//
-//    private func setFoodLabels(_ weekDay:WeekDay) {
-//        if let day = WeekViewModel.setDay(from: weekDay.date) {
-//            if let breakfast = day.getMeal(tipo: .breakfast) {
-//                lbl_breakfast.text = breakfast.getFoodNames()
-//            } else {
-//                lbl_breakfast.text = "No breakfast added"
-//            }
-//
-//            if let lunch = day.getMeal(tipo: .lunch) {
-//                lbl_lunch.text = lunch.getFoodNames()
-//            } else {
-//                lbl_lunch.text = "No lunch added"
-//            }
-//
-//            if let dinner = day.getMeal(tipo: .dinner) {
-//                lbl_dinner.text = dinner.getFoodNames()
-//            } else {
-//                lbl_dinner.text = "No dinner added"
-//            }
-//        } else {
-//            setDefaultValues()
-//        }
-//    }
-//
-//    private func setDefaultValues() {
-//        lbl_breakfast.text = "No breakfast added"
-//        lbl_lunch.text = "No lunch added"
-//        lbl_dinner.text = "No dinner added"
-//        circleView_FoodType.alpha = 0;
-//    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
 
-    // jaja soy malo
+    #warning("No implementation of circleView_FoodType")
+    // Cell genereated in the week view
+    func setCell(with weekDay:WeekDay, _ color: UIColor?) {
+        self.selectionStyle = .none
+        view_day.cornerRadius(de: 25)
+        view_day.backgroundColor = color
+        circleView_FoodType.circle = true
+        setFoodLabels(weekDay)
+        setImages()
+        lbl_day.text = weekDay.dateDay + ", " + weekDay.date.dayMonthDate
+    }
+
+    private func setImages() {
+        im_lunch.image = UIImage(named: "Sun2.png")
+        im_dinner.image = UIImage(named: "Moon2.png")
+        im_breakfast.image = UIImage(named: "Sunrise2.png")
+    }
+
+    private func setFoodLabels(_ weekDay:WeekDay) {
+        if let day = WeekViewModel.setDay(from: weekDay.date) {
+            if let breakfast = day.getMeal(tipo: .breakfast) {
+                lbl_breakfast.text = breakfast.getFoodNames()
+            } else {
+                lbl_breakfast.text = "No breakfast added"
+            }
+
+            if let lunch = day.getMeal(tipo: .lunch) {
+                lbl_lunch.text = lunch.getFoodNames()
+            } else {
+                lbl_lunch.text = "No lunch added"
+            }
+
+            if let dinner = day.getMeal(tipo: .dinner) {
+                lbl_dinner.text = dinner.getFoodNames()
+            } else {
+                lbl_dinner.text = "No dinner added"
+            }
+        } else {
+            setDefaultValues()
+        }
+    }
+
+    private func setDefaultValues() {
+        lbl_breakfast.text = "No breakfast added"
+        lbl_lunch.text = "No lunch added"
+        lbl_dinner.text = "No dinner added"
+        circleView_FoodType.alpha = 0;
+    }
+
+     jaja soy malo
 }
